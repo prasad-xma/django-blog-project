7c32e6e260646712ce80ab8e200f365998f9b46d
Prasad Manamperi <prasadmanamperi130@gmail.com>
2025-08-05 23:43:57 +0530

    add login and register views

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .decorators import roles_required
+from django.contrib.auth import login, logout, authenticate
+from .forms import CustomUserCreationForm, LoginForm
 
 # Create your views here.
 
@@ -9,3 +11,46 @@ def home(request):
 @roles_required('admin')
 def admin_dashboard(request):
     return render(request, 'admin_dashboard.html')
+
+
+# register
+def register_view(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            
+            user.role = 'user'
+            
+            # save user
+            user.save()
+            
+            return redirect('login')
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'register.html', {'form': form})
+    
+# login view
+def login_view(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+            
+            if user:
+                login(request, user)
+                # redirect by role
+                if user.role == 'admin':
+                    return redirect('admin_dashboard')
+                else:
+                    return redirect('home')
+            else:
+                form.add_error(None, "Invalid credentials")
+    else:
+        form = LoginForm()
+    return render(request, 'login.html', {'form': form})
+    
+
