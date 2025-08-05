68a355ea016be6ca5c07b4e3a3f100d475ca572d
Prasad Manamperi <prasadmanamperi130@gmail.com>
2025-08-05 06:49:56 +0530

    add postgre db connection

--- blog_app/settings.py
+++ blog_app/settings.py
@@ -74,8 +74,12 @@ WSGI_APPLICATION = 'blog_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mydjangodb',
+        'USER': 'postgres',
+        'PASSWORD': 'blogapp123',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
