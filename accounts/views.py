40a12c82c40a1fc024af13deed12ad9242d1ee1e
Prasad Manamperi <prasadmanamperi130@gmail.com>
2025-08-05 20:12:29 +0530

    create home and admin views

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .decorators import roles_required
 
 # Create your views here.
+
+def home(request):
+    return render(request, 'home.html')
+
+@roles_required('admin')
+def admin_dashboard(request):
+    return render(request, 'admin_dashboard.html')
