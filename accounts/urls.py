c3b3ad5ef635b5f38914e6296af4696c55868875
Prasad Manamperi <prasadmanamperi130@gmail.com>
2025-08-07 04:28:43 +0530

    create profile function

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('admin-dash/', views.admin_dashboard, name='admin_dashboard'),
     path('login/', views.login_view, name='login'),
     path('register/', views.register_view, name='register'),
-    path('logout/', views.logout_view, name='logout')
+    path('logout/', views.logout_view, name='logout'),
+    # user profile
+    path('profile/<str:username>/', views.profile_view, name='profile')
 ]
