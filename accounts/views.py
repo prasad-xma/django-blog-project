0c0d9ae81f6f4c21331bf61a4cf38a48926a8ab5
Prasad Manamperi <prasadmanamperi130@gmail.com>
2025-08-07 20:20:32 +0530

    add: profile edit view

--- accounts/views.py
+++ accounts/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render, redirect
 from .decorators import roles_required
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login, logout, authenticate
-from .forms import CustomUserCreationForm, LoginForm
+from .forms import CustomUserCreationForm, LoginForm, EditProfileForm
 from django.http import HttpResponseForbidden
+from django.contrib import messages
 
 # Create your views here.
 
@@ -68,7 +69,7 @@ def profile_view(request, username):
         return HttpResponseForbidden("You are not authorized to view this page!")
     return render(request, 'profile.html', {'user': request.user})
     
-        
+          
         
 # logout view
 @login_required
@@ -76,3 +77,23 @@ def logout_view(request):
     logout(request)
     return redirect('home')
 
+
+# profile edit view
+@login_required
+def profile_edit_view(request, username):
+    if request.user.username != username:
+        return HttpResponseForbidden("You are not authorized to view this page!")
+    
+    user = request.user
+    
+    if request.method == 'POST':
+        form = EditProfileForm(request.POST, instance=user)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Profile updated successfully!")
+            return redirect('profile_view', username=user.username)
+    else:
+        form = EditProfileForm(instance=user)
+    
+    return render(request, 'edit_profile.html', {'form': form})
+    
