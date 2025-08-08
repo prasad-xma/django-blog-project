372394bdf80a7282e9c3e673f75e989dc6eaa0fc
Prasad Manamperi <prasadmanamperi130@gmail.com>
2025-08-08 11:50:29 +0530

    create urls file in blogs

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('accounts.urls')),
-    path('', include('blogs.urls')),
+    # path('', include('blogs.urls')),
 ]
 
 if settings.DEBUG:
