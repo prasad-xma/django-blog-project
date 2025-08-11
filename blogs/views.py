3b01264334cf6cfee58b7991e5177e7a8a60e2b0
Prasad Manamperi <prasadmanamperi130@gmail.com>
2025-08-12 03:19:47 +0530

    update: additional imports for blog functionality

--- blogs/views.py
+++ blogs/views.py
@@ -1,6 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseForbidden
+from django.contrib import messages
+from .models import Blog
+
 
 # Create your views here.
 
 def home(request):
     return render(request, 'home.html')
+
