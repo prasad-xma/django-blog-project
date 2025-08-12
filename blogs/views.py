d2dd835424a5897dc0eba10e1d26da16dad23d60
Prasad Manamperi <prasadmanamperi130@gmail.com>
2025-08-13 01:15:39 +0530

    create: user blog view

--- blogs/views.py
+++ blogs/views.py
@@ -16,6 +16,10 @@ def blog_list(request):
     blogs = Blog.objects.filter(status='public')
     return render(request, 'blogs/blog_list.html', {'blogs': blogs})
 
+# get user's blog
+def my_blog(request):
+    blogs = Blog.objects.filter(owner=request.user)
+    return render(request, 'blogs/my_blog.html', {'blogs': blogs})
 
 # blog detail
 def blog_detail(request, pk):
