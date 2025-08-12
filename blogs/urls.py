3a255888fbb3edc9a759fa228cc4b1ae1fefa04c
Prasad Manamperi <prasadmanamperi130@gmail.com>
2025-08-12 10:26:40 +0530

    fix: blog url

--- blogs/urls.py
+++ blogs/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('create/', views.blog_create, name='blog_create'),
     path('<int:pk>/', views.blog_detail, name='blog_detail'),
     path('<int:pk>/edit/', views.blog_edit, name='blog_edit'),
-    path('<int:pk>/delete/', views.blog_detail, name='blog_delete'),
+    path('<int:pk>/delete/', views.blog_delete, name='blog_delete'),
 ]
