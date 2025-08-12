849a94d9d51b6cb7d6effed481a6b205ab18cd70
Prasad Manamperi <prasadmanamperi130@gmail.com>
2025-08-12 13:24:07 +0530

    update: blog form

--- blogs/forms.py
+++ blogs/forms.py
@@ -4,4 +4,4 @@ from .models import Blog
 class BlogForm(forms.ModelForm):
     class Meta:
         model = Blog
-        fields = ['title', 'content', 'image', 'status', 'tags']
+        fields = ['title', 'content', 'status', 'image', 'tags']
