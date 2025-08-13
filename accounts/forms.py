978242395992322bd56fdfa88609bba594243d36
Prasad Manamperi <prasadmanamperi130@gmail.com>
2025-08-14 02:40:40 +0530

    update: change class style

--- accounts/forms.py
+++ accounts/forms.py
@@ -26,13 +26,13 @@ class CustomUserCreationForm(UserCreationForm):
 class LoginForm(forms.Form):
     username = forms.CharField(
         widget=forms.TextInput(attrs={
-            'class': 'form-control form-control-lg',
+            'class': 'form-control',
             'placeholder': 'Enter your username'
         })
     )
     password = forms.CharField(
         widget=forms.PasswordInput(attrs={
-            'class': 'form-control form-control-lg',
+            'class': 'form-control',
             'placeholder': 'Enter your password'
         })
     )
