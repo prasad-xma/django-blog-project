11cf89c15be339fce1b2b66340cd7ed4da0eeeb3
Prasad Manamperi <prasadmanamperi130@gmail.com>
2025-08-14 02:04:55 +0530

    update: add register form widgets

--- accounts/forms.py
+++ accounts/forms.py
@@ -7,6 +7,14 @@ class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'first_name', 'last_name', 'password1', 'password2']
+        widgets = {
+            'username': forms.TextInput(attrs={'class': 'form-control'}),
+            'first_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'last_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control'}),
+            'password1': forms.PasswordInput(attrs={'class': 'form-control form-control-lg'}),
+            'password2': forms.PasswordInput(attrs={'class': 'form-control form-control-lg'})
+        }
         
 
 # login form
